9303fc8fbbcee94d43b420447f478586f7570b99
001damege <chayama.koji0401@gmail.com>
2025-05-15 22:00:40 +0900

    Mainクラスのいらなそうなものを削除

--- src/main/java/com/mikazukichandamege/reinforcedae/ReinforcedAE.java
+++ src/main/java/com/mikazukichandamege/reinforcedae/ReinforcedAE.java
@@ -1,15 +1,9 @@
 package com.mikazukichandamege.reinforcedae;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
@@ -18,42 +12,10 @@ import org.slf4j.Logger;
 public class ReinforcedAE {
 
     public static final String MOD_ID = "reinforcedae";
-
-    private static final Logger LOGGER = LogUtils.getLogger();
-
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public ReinforcedAE() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-
-        modEventBus.addListener(this::commonSetup);
-
-
         MinecraftForge.EVENT_BUS.register(this);
-
-
-        modEventBus.addListener(this::addCreative);
-
-
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event) {
-    }
-
-
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-    }
-
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-    }
-
-
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-        }
     }
 }
