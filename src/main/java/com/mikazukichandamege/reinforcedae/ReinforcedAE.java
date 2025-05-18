26064c30de8ab822d0763da895e77514e5bfa4f8
001damege <chayama.koji0401@gmail.com>
2025-05-18 19:54:37 +0900

    tterrag使えたら楽そう

--- src/main/java/com/mikazukichandamege/reinforcedae/ReinforcedAE.java
+++ src/main/java/com/mikazukichandamege/reinforcedae/ReinforcedAE.java
@@ -1,7 +1,9 @@
 package com.mikazukichandamege.reinforcedae;
 
 import com.mojang.logging.LogUtils;
+import com.tterrag.registrate.Registrate;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.Lazy;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -13,9 +15,14 @@ public class ReinforcedAE {
 
     public static final String MOD_ID = "reinforcedae";
     public static final Logger LOGGER = LogUtils.getLogger();
+    private static final Lazy<Registrate> REGISTRATE = Lazy.of(() -> Registrate.create(MOD_ID));
 
     public ReinforcedAE() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    public static Registrate registrate() {
+        return REGISTRATE.get();
     }
 }
